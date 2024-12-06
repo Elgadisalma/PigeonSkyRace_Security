caf39c53762c5f6192b3464aa45143e0e0aea097
Elgadisalma <elgadi.salma0@gmail.com>
2024-12-06 11:59:55 +0100

    PIG-3 configure spring security

--- pigeon/src/main/java/com/example/pigeon/security/SecurityConfig.java
+++ pigeon/src/main/java/com/example/pigeon/security/SecurityConfig.java
@@ -1,36 +1,65 @@
 package com.example.pigeon.security;
 
 
+
+import com.example.pigeon.exception.CustomAccessDeniedHandler;
+import com.example.pigeon.exception.CustomAuthenticationEntryPoint;
+import com.example.pigeon.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
-
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http  .authorizeHttpRequests(auth -> auth
-//                .requestMatchers(new AntPathRequestMatcher("/api/users/register")).permitAll()
-//                .anyRequest().authenticated()
-//        )
-//                .httpBasic(httpBasic -> httpBasic.withDefaults())
-//                .sessionManagement(session -> session
-//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                )
-//                .csrf(csrf -> csrf.disable());
-//
-//        return http.build();
-//    }
+    @Autowired
+   CustomAuthenticationProvider customAuthenticationProvider;
+    @Autowired
+    CustomAccessDeniedHandler customAccessDeniedHandler;
+    @Autowired
+    CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
+                        .requestMatchers("/api/users/{userId}/role","/api/admin/users").hasRole("ADMIN")
+                        .requestMatchers("/api/competition/add", "/api/competition/**", "/api/resultats/**").hasAnyRole("ORGANIZER", "ADMIN")
+                        .requestMatchers("/api/pigeons/add").hasRole("USER")
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(customAuthenticationProvider)
+                .exceptionHandling(exception -> exception
+                        .accessDeniedHandler(customAccessDeniedHandler)
+                        .authenticationEntryPoint(customAuthenticationEntryPoint)
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
     }
+
+
+
+
+
+
+
 }
